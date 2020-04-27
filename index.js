b69be78acd67c7a39fef8cde40a7e77ef1b65df0
Michael Heap <m@michaelheap.com>
2020-04-27 19:58:14 +0100

    Fix issue being added - was a deprecated field

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const { Toolkit } = require('actions-toolkit')
-const { Octokit } = require("@octokit/rest");
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-
-  tools.github = new Octokit({
-    "auth": process.env.GITHUB_TOKEN,
-    log: {
-      debug: console.log,
-      info: console.log,
-      warn: console.warn,
-      error: console.error
-    }
-  })
-
-  tools.log.info({
-    ...tools.context.issue,
-    labels: ["needs-triage"]
-  })
-
   await tools.github.issues.addLabels({
-    ...tools.context.issue,
+    ...tools.context.repo,
+    issue_number: tools.context.issue.number,
     labels: ["needs-triage"]
   });
 
