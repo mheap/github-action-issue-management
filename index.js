4547a6edd4d6ea577accf849e3e91f3793438330
Michael Heap <m@michaelheap.com>
2020-04-27 20:04:57 +0100

    Start adding issue comment detection

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const { Toolkit } = require('actions-toolkit')
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  await tools.github.issues.addLabels({
-    ...tools.context.repo,
-    issue_number: tools.context.issue.number,
-    labels: ["needs-triage"]
-  });
+
+  // Add a triage label to new pull requests
+  if (tools.context.payload.pull_request.action == "opened") {
+    await addLabels(tools, ["needs-triage"]);
+    return;
+  }
+
+  // Update labels to show that it's being triaged when someone
+  // with the correct permissions comments. Permission could mean
+  // being in the correct team, or having write+ access
+  if (tools.context.payload.issue_comment.action == "created") {
+    tools.log.info("Comment added")
+  }
 
   tools.exit.success("Issue managed!")
 })
+
+function addLabels(tools, labels) {
+  return tools.github.issues.addLabels({
+    ...tools.context.repo,
+    issue_number: tools.context.issue.number,
+    labels
+  });
+}
