7448fbbea37de0262a424ca9cfde8b754bbc6ab1
Michael Heap <m@michaelheap.com>
2020-04-27 20:30:23 +0100

    Permissions check

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Toolkit } = require('actions-toolkit')
 // Run your GitHub Action!
 Toolkit.run(async tools => {
 
-  console.log(tools.context.event)
+  console.log(tools.context)
 
   // Add a triage label to new pull requests
   if (tools.context.event == "pull_request" && tools.context.payload.action == "opened") {
@@ -15,15 +15,21 @@ Toolkit.run(async tools => {
   // with the correct permissions comments. Permission could mean
   // being in the correct team, or having write+ access
   if (tools.context.event == "issue_comment" && tools.context.payload.action == "created") {
+    console.log("Running on issue comment")
+
+    const allowed = ["write", "admin"];
+
     const perms = (await tools.github.repos.getCollaboratorPermissionLevel({
       ...tools.context.repo,
       username: tools.context.actor
-    })).data;
+    }));
 
     console.log(perms);
 
-    // await removeLabels(tools, ["needs-triage"]);
-    // await addLabels(tools, ["under-triage"]);
+    if (allowed.includes(perms.permission)) {
+      await removeLabels(tools, ["needs-triage"]);
+      await addLabels(tools, ["under-triage"]);
+    }
   }
 
   tools.exit.success("Issue managed!")
