e36040d0d3f7572f4130a86948f9c7baaf2f2996
Michael Heap <m@michaelheap.com>
2020-04-27 19:54:03 +0100

    Add Octokit debugging

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const { Toolkit } = require('actions-toolkit')
+const { Octokit } = require("@octokit/rest");
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
+
+  tools.github = new Octokit({
+    "auth": process.env.GITHUB_TOKEN,
+    log: {
+      debug: console.log,
+      info: console.log,
+      warn: console.warn,
+      error: console.error
+    }
+  })
+
+  tools.log.info({
+    ...tools.context.issue,
+    labels: ["needs-triage"]
+  })
 
   await tools.github.issues.addLabels({
     ...tools.context.issue,
