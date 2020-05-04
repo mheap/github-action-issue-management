de86ecaabd47b13406ea1dba6ae8780ede0e92f3
Michael Heap <m@michaelheap.com>
2020-05-04 19:13:12 +0100

    Add outline of use cases

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const { Toolkit } = require("actions-toolkit");
 const router = require("@mheap/action-router");
-const { Octokit } = require("@octokit/rest");
+
+/*
+ * When an issue or a PR is opened, add the needs-triage label
+ * When someone with write+ (configurable) access comments, add waiting-for-author label
+ * When OP comments, add awaiting-reply label
+ * When someone other than OP or maintainer comments, add needs-reply label
+ * When PR gets new commits, add updated-commits label
+ * When PR is closed, remove all labels and add closed-by-op, closed-by-team labels
+ */
 
 // Run your GitHub Action!
 Toolkit.run(async (tools) => {
   await router(
     {
       // Add a triage label to new pull requests
+      "issue.opened": onPrOpened,
       "pull_request.opened": onPrOpened,
       "issue_comment.created": onIssueComment,
     },
